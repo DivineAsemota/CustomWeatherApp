8f23d9fb5ce861fb3b05dde0a273a555e674b204
DEDANDEUS <114673700+DEDANDEUS@users.noreply.github.com>
2023-10-17 10:15:52 -0400

    weather repository

--- WeatherApp-Backend/weatherApp/src/main/java/com/bptn/weatherApp/repository/WeatherRepository.java
+++ WeatherApp-Backend/weatherApp/src/main/java/com/bptn/weatherApp/repository/WeatherRepository.java
@@ -1,7 +1,12 @@
 package com.bptn.weatherApp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WeatherRepository extends JpaRepository<T, ID> {
+import com.bptn.weatherApp.jpa.User;
+import com.bptn.weatherApp.jpa.Weather;
 
+public interface WeatherRepository extends JpaRepository<Weather, Integer> {
+	List<Weather> findFirst10ByUserOrderByWeatherIdDesc(User user);
 }

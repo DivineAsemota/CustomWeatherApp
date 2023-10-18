b16c8b6107ad17fd7e15ee4efe0506da2a134e29
Divine Asemota <divineasemota125@gmail.com>
2023-10-18 10:03:43 -0400

    changes made to UserController

---
+++ WeatherApp-Backend/weatherApp/src/main/java/com/bptn/weatherApp/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.bptn.weatherApp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bptn.weatherApp.jpa.User;
+import com.bptn.weatherApp.service.UserService;
+
+@CrossOrigin(exposedHeaders = "Authorization")
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired 
+	UserService userService;
+	
+	@PostMapping("/signup")
+	public User signup(@RequestBody User user) {	
+		logger.debug("Signing up, username: {}", user.getUsername());
+		return this.userService.signup(user);
+	}
+}

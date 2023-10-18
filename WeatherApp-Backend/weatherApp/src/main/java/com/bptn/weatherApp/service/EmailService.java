98d88bd96ae921b49dcc6e066fe1dca5e2e273c7
EliasRoro <eliassaychelem@gmail.com>
2023-10-18 14:44:48 -0400

    RestPassword feature added

--- WeatherApp-Backend/weatherApp/src/main/java/com/bptn/weatherApp/service/EmailService.java
+++ WeatherApp-Backend/weatherApp/src/main/java/com/bptn/weatherApp/service/EmailService.java
@@ -1,4 +1,5 @@
 package com.bptn.weatherApp.service;
+
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,65 +18,69 @@ import org.springframework.scheduling.annotation.Async;
 @Service
 public class EmailService {
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Value("${spring.mail.username}")
 	private String emailFrom;
-	
+
 	@Autowired
 	JwtService jwtService;
-		
+
 	@Autowired
 	ResourceProvider provider;
 
 	@Autowired
 	TemplateEngine templateEngine;
-		
+
 	@Autowired
-	JavaMailSender javaMailSender;	
-	
-private void sendEmail(User user, String clientParam, String templateName, String emailSubject, long expiration) {
-		
+	JavaMailSender javaMailSender;
+
+	private void sendEmail(User user, String clientParam, String templateName, String emailSubject, long expiration) {
+
 		try {
-			
+
 			/* Collect Data for the Email HTML generation */
 			Context context = new Context();
 			context.setVariable("user", user);
-			context.setVariable("client", this.provider.getClientUrl());			
+			context.setVariable("client", this.provider.getClientUrl());
 			context.setVariable("param", clientParam);
-			context.setVariable("token", this.jwtService.generateJwtToken(user.getUsername(),expiration));
-			
+			context.setVariable("token", this.jwtService.generateJwtToken(user.getUsername(), expiration));
+
 			/* Process Email HTML Template */
 			String process = this.templateEngine.process(templateName, context);
-			
+
 			MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
 			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-			
+
 			/* Set Email Information */
 			helper.setFrom(this.emailFrom, "WeatherApp - Obsidi Academy");
 			helper.setSubject(emailSubject);
 			helper.setText(process, true);
 			helper.setTo(user.getEmailId());
-			
+
 			/* Send Email */
 			this.javaMailSender.send(mimeMessage);
-			
+
 			this.logger.debug("Email Sent, {} ", user.getEmailId());
 
 		} catch (Exception ex) {
-			
+
 			this.logger.error("Error while Sending Email, Username: " + user.getUsername(), ex);
 		}
-}	
-@Async
-public void sendVerificationEmail(User user) {
-		
-	this.sendEmail(user, this.provider.getClientVerifyParam(),"verify_email", 
-	String.format("Welcome %s %s",user.getFirstName(),user.getLastName()), 
-				      this.provider.getClientVerifyExpiration());
-}	
-@Async
-public void sendResetPasswordEmail(User user) {
-		
-	this.sendEmail(user, this.provider.getClientResetParam(), "reset_password", "Reset your password", this.provider.getClientResetExpiration());
-}	
+	}
+
+	@Async
+	public void sendVerificationEmail(User user) {
+
+		this.sendEmail(user, this.provider.getClientVerifyParam(), "verify_email",
+				String.format("Welcome %s %s", user.getFirstName(), user.getLastName()),
+				this.provider.getClientVerifyExpiration());
+	}
+
+	@Async
+	public void sendResetPasswordEmail(User user) {
+
+		this.sendEmail(user, this.provider.getClientResetParam(), "reset_password", "Reset your password",
+				this.provider.getClientResetExpiration());
+	}
+
 }

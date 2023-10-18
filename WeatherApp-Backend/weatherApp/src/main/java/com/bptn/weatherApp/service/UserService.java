e002d4208c03fa17edfe7ada92439c61a32bb898
Divine Asemota <divineasemota125@gmail.com>
2023-10-18 09:39:06 -0400

    Creation of UserService

---
+++ WeatherApp-Backend/weatherApp/src/main/java/com/bptn/weatherApp/service/UserService.java
@@ -0,0 +1,60 @@
+package com.bptn.weatherApp.service;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.bptn.weatherApp.exception.domain.EmailExistException;
+import com.bptn.weatherApp.exception.domain.UsernameExistException;
+import com.bptn.weatherApp.jpa.User;
+import com.bptn.weatherApp.repository.UserRepository;
+
+@Service
+public class UserService {
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	EmailService emailService;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	private void validateUsernameAndEmail(String username, String emailId) {
+
+		this.userRepository.findByUsername(username).ifPresent(u -> {
+			throw new UsernameExistException(String.format("Username already exists, %s", u.getUsername()));
+		});
+
+		this.userRepository.findByEmailId(emailId).ifPresent(u -> {
+			throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));
+		});
+
+}
+	public User signup(User user) {
+
+		user.setUsername(user.getUsername().toLowerCase());
+		user.setEmailId(user.getEmailId().toLowerCase());
+
+		this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
+
+		user.setEmailVerified(false);
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+	
+		user.setCreatedOn(Timestamp.from(Instant.now()));
+
+	    this.userRepository.save(user);
+
+		this.emailService.sendVerificationEmail(user);
+		
+		return user;
+	}
+
+}
